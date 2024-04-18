80461ebc6fa8e406b48a435c97e066ef46ed49cf
vipanshijindal <vipanshijindal008@gmail.com>
2024-04-18 19:23:09 +0530

    changed path

--- src/vendorDashboard/data/ApiPath.js
+++ src/vendorDashboard/data/ApiPath.js
@@ -1,4 +1,3 @@
- export const API_URL= "http://localhost:4000"
-//  export const API_URL= "https://backend-hungries.onrender.com"
+//  export const API_URL= "http://localhost:4000"
+ export const API_URL= "https://backend-hungries.onrender.com"
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZW5kb3JJZCI6IjY2MWZiZTg5NDIxNzM1ZjIwZWVlM2I1NiIsImlhdCI6MTcxMzM1NjQ2NywiZXhwIjoxNzEzMzYwMDY3fQ.agcBuEGZ-goRX7ohX7Sb2ZeKUk5LXoyeR9KwEO39QTE
